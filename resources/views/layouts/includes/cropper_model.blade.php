6796262e18fd43de54d559b9ed45f343f63e15ae
navdeep kumar <navdeepkumar24493@gmail.com>
2022-05-11 18:36:22 +0530

    fixed cropper

--- resources/views/layouts/includes/cropper_model.blade.php
+++ resources/views/layouts/includes/cropper_model.blade.php
@@ -1,6 +1,6 @@
 <div class="modal fade"  id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content" >
+        <div class="modal-content" style="height: 85vh;overflow: auto;" >
             <div class="modal-header">
                 <h5 class="modal-title" id="modalLabel">Edit Picture</h5>
                 <button type="button" class="close cancel" data-dismiss="modal" aria-label="Close">
